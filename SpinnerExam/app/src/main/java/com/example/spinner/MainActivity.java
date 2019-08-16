cd3d8122fd60f9f0d56a0df284a692bd926909c5
mengxyz <meng348@gmail.com>
2019-08-16 10:07:29 +0700

    Add bind String Array

--- SpinnerExam/app/src/main/java/com/example/spinner/MainActivity.java
+++ SpinnerExam/app/src/main/java/com/example/spinner/MainActivity.java
@@ -5,12 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,21 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<String> arrList = new ArrayList<String>();
-        arrList.add("Mocca");
-        arrList.add("Late");
-        arrList.add("Arabica");
-        arrList.add("Esspresso");
-        arrList.add("Capuchino");
-        final ArrayAdapter<String> arrAd = new ArrayAdapter<String>(MainActivity.this,R.layout.support_simple_spinner_dropdown_item,arrList);
-        Spinner spinner =  findViewById(R.id.spinner1);
 
-        spinner.setAdapter(arrAd);
+        final Spinner spinner = findViewById(R.id.spinner1);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this,"Your Selected : "+arrAd.getItem(i),Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"Your Selected : "+spinner.getSelectedItem(),Toast.LENGTH_SHORT).show();
             }
 
             @Override
